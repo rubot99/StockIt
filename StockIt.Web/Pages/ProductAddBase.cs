18fb9d0ed441a4696ac0aa990b003dd3ae5d7d26
rubot99 <rubot99@gmail.com>
2020-10-31 22:24:09 +0000

    Added product page

---
+++ StockIt.Web/Pages/ProductAddBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using StockIt.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockIt.Web.Pages
+{
+    public class ProductAddBase : ComponentBase
+    {
+        protected Product product;
+
+        protected override async Task OnInitializedAsync()
+        {
+            product = new Product();
+        }
+
+        protected async Task HandleSubmit()
+        { }
+    }
+}
---
+++ StockIt.Web/Pages/ProductListBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using StockIt.Core.Models;
+using StockIt.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockIt.Web.Pages
+{
+    public class ProductListBase : ComponentBase
+    { 
+        protected List<Product> products;
+
+        protected override async Task OnInitializedAsync()
+        {
+            products = new List<Product>
+            {
+                new Product
+                {
+                    Id = "kkk"
+                },
+                new Product
+                {
+                    Id = "kkkikpkpo"
+                }
+            };
+        }
+    }
+}
