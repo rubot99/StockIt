27dcc196fe64e7b145d36bc3fa5fe4f0a7dba30f
rubot99 <rubot99@gmail.com>
2020-11-14 14:12:40 +0000

    Updates

---
+++ StockIt.Web/Pages/StockItemBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using StockIt.Core.Repositories.Location;
+using StockIt.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockIt.Web.Pages
+{
+    public class StockItemBase : ComponentBase
+    {
+        protected bool saved = false;
+        protected string message;
+        protected string statusClass;
+
+        public Location Location { get; set; }
+        public string ActionType { get; set; }
+        public StockModel StockModel { get; set; }
+
+        protected async Task HandleValidSubmit()
+        {
+        }
+    }
+}
---
+++ StockIt.Web/ViewModels/StockModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockIt.Web.ViewModels
+{
+    public class StockModel
+    {
+        public StockItActionType ActionType { get; set; }
+    }
+
+    public enum StockItActionType { ToStock, FromtStock, MoveStock };
+}
