e2939cd7f9680736c1d2d24e379f5dfc657455ff
rubot99 <rubot99@gmail.com>
2020-11-14 22:39:06 +0000

    update

--- StockIt.Core/Repositories/DocumentStoreHolder.cs
+++ StockIt.Core/Repositories/DocumentStoreHolder.cs
@@ -1,5 +1,6 @@
 ﻿using Raven.Client.Documents;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace StockIt.Core.Repositories
 {
@@ -9,13 +10,14 @@ namespace StockIt.Core.Repositories
 
         private static IDocumentStore CreateDocumentStore()
         {
-            string serverURL = "http://localhost:8080";
+            string serverURL = "https://a.faraday.development.run/";
             string databaseName = "stockit";
 
             IDocumentStore documentStore = new DocumentStore
             {
                 Urls = new[] { serverURL },
-                Database = databaseName
+                Database = databaseName,
+                Certificate = new X509Certificate2(@"C:\Users\reube\OneDrive\Documents\Certs\stockitweb\stockitweb.pfx"),
             };
 
             documentStore.Initialize();
